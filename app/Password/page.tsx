dba3c4a7aa07e6fe88b53b2d112cb68483d155fa
euaragon <eugenio.aragon@gmail.com>
2023-07-19 23:10:29 -0300

    componente password para cajero

--- app/Password/page.tsx
+++ app/Password/page.tsx
@@ -6,8 +6,10 @@ export default function page() {
 
   return (
     <main>
-        <h2>Recuperar Contraseña</h2>
-        <input type="email" placeholder='email' />
+        <h2>Restablecer Nueva Contraseña</h2>
+        <input type="password" placeholder='Contraseña Anterior' />
+        <input type="password" placeholder='Nueva Contraseña' />
+        <input type="password" placeholder='Repetir Contraseña' />
         <button>Enviar</button>
         <a href={"/"}>volver</a>
     </main>
