6285bc725904efc75838149a8b07eb9eaa0bc306
euaragon <eugenio.aragon@gmail.com>
2023-07-24 20:42:16 -0300

    formulario controlado crear cajero

--- app/Components/CreateCashier/CreateCashier.tsx
+++ app/Components/CreateCashier/CreateCashier.tsx
@@ -1,8 +1,6 @@
-"use client";
 import React, { useState } from "react";
 import css from "./CreateCashier.module.css";
-import swal from 'sweetalert';
-
+import swal from "sweetalert";
 
 const CreateCashier = ({ tokenId, onClose, reload }) => {
   const [input, setInput] = useState({
@@ -12,15 +10,32 @@ const CreateCashier = ({ tokenId, onClose, reload }) => {
     percent_agreement: 0,
   });
 
-  const createCashier = async (token:string, object:object) => {
-    const userDb = await fetch("http://localhost:3001/users", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        authorization: "Bearer " + token,
-      },
-      body: JSON.stringify(object),
-    });
+  const [errors, setErrors] = useState({
+    username: "",
+    phone: "",
+    email: "",
+    percent_agreement: "",
+  });
+
+  const createCashier = async (token, object) => {
+    try {
+      const response = await fetch("http://localhost:3001/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: "Bearer " + token,
+        },
+        body: JSON.stringify(object),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al crear el cajero.");
+      }
+
+      return response.json();
+    } catch (error) {
+      throw new Error(error.message);
+    }
   };
 
   const handlerInputChange = ({ target: { name, value } }) => {
@@ -30,31 +45,69 @@ const CreateCashier = ({ tokenId, onClose, reload }) => {
     });
   };
 
-
-  const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      createCashier(tokenId, input);
-    } catch (error) {
-      swal(error.message);
-    }
-    swal({
-      title: 'Cajero creado correctamente!',
-      icon: 'success',
-
-    });
-
-    setInput({
+  const validateForm = () => {
+    let hasErrors = false;
+    const newErrors = {
       username: "",
       phone: "",
       email: "",
-      percent_agreement: 0,
-    });
-    onClose();
-    reload();
+    percent_agreement: "",
+    };
+
+    if (!input.username) {
+      newErrors.username = "Debe ingresar un nombre de usuario.";
+      hasErrors = true;
+    }
+
+    if (!input.phone) {
+      newErrors.phone = "Debe ingresar un número de celular.";
+      hasErrors = true;
+    }
+
+    if (!input.email) {
+      newErrors.email = "Debe ingresar un email.";
+      hasErrors = true;
+    }
+
+    if (input.percent_agreement === 0 || !input.percent_agreement) {
+      newErrors.percent_agreement = "Debe ingresar un monto mayor a 0.";
+      hasErrors = true;
+    }
+
+    setErrors(newErrors);
+    return !hasErrors;
   };
 
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
 
+    if (validateForm()) {
+      try {
+        await createCashier(tokenId, input);
+
+        setInput({
+          username: "",
+          phone: "",
+          email: "",
+          percent_agreement: 0,
+        });
+
+        onClose();
+        reload();
+
+        swal({
+          title: "Cajero creado correctamente!",
+          icon: "success",
+        });
+      } catch (error) {
+        swal({
+          title: "Error",
+          text: error.message,
+          icon: "error",
+        });
+      }
+    }
+  };
 
   return (
     <div className={css.container}>
@@ -70,16 +123,16 @@ const CreateCashier = ({ tokenId, onClose, reload }) => {
           placeholder="Usuario"
           value={input.username}
           onChange={handlerInputChange}
-          required
         />
+        {errors.username && <p className={css.errorMsg}>{errors.username}</p>}
         <input
           type="number"
           name="phone"
           value={input.phone}
           placeholder="Numero de Celular"
           onChange={handlerInputChange}
-          required
         />
+        {errors.phone && <p className={css.errorMsg}>{errors.phone}</p>}
         <input
           type="email"
           name="email"
@@ -87,14 +140,15 @@ const CreateCashier = ({ tokenId, onClose, reload }) => {
           placeholder="Email"
           onChange={handlerInputChange}
         />
+        {errors.phone && <p className={css.errorMsg}>{errors.phone}</p>}
         <input
           type="number"
-          min={1}
           name="percent_agreement"
           value={input.percent_agreement}
           placeholder="Porcentaje"
           onChange={handlerInputChange}
         />
+        {errors.percent_agreement && <p className={css.errorMsg}>{errors.percent_agreement}</p>}
         <button className={css.btn} type="submit">
           Crear cajero
         </button>
