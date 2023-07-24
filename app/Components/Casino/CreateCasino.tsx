ac6739b97d60755bb32ac97ba48d17f7a464d915
euaragon <eugenio.aragon@gmail.com>
2023-07-24 20:27:21 -0300

    formulario controlado crear casino

--- app/Components/Casino/CreateCasino.tsx
+++ app/Components/Casino/CreateCasino.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState } from "react";
 import { useUserContext } from "../../UserContext/UserContext";
 import swal from "sweetalert";
@@ -7,13 +6,20 @@ import css from "./CreateCasino.module.css";
 const CreateCasino = ({ onClose, Reload }) => {
   const { userDb } = useUserContext();
   const tokenId = userDb?.token;
+
   const [input, setInput] = useState({
     name: "",
     image_url: "",
   });
-  const postCasino = async (obj: object, token: string) => {
+
+  const [errors, setErrors] = useState({
+    name: "",
+    image_url: "",
+  });
+
+  const postCasino = async (obj, token) => {
     try {
-      const userDb = await fetch("http://localhost:3001/casino", {
+      const response = await fetch("http://localhost:3001/casino", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -21,51 +27,99 @@ const CreateCasino = ({ onClose, Reload }) => {
         },
         body: JSON.stringify(obj),
       });
+
+      if (!response.ok) {
+        throw new Error("Error al crear el casino.");
+      }
+
+      return response.json();
     } catch (error) {
-      console.log(error.message);
+      throw new Error(error.message);
     }
   };
-  const handlerInputChange = ({
-    target: { value, name },
-  }: React.ChangeEvent<HTMLInputElement>) => {
+
+  const handlerInputChange = (e) => {
+    const { name, value } = e.target;
     setInput({
       ...input,
       [name]: value,
     });
   };
-  const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    postCasino(input, tokenId);
-    setInput({
+
+  const validateForm = () => {
+    let hasErrors = false;
+    const newErrors = {
       name: "",
       image_url: "",
-    });
-    onClose();
-    Reload();
-    swal({
-      title: 'Casino creado correctamente!',
-      icon: 'success',
+    };
 
-    });
+    if (!input.name) {
+      newErrors.name = "Debe ingresar un nombre de casino.";
+      hasErrors = true;
+    }
+
+    if (!input.image_url) {
+      newErrors.image_url = "Debe ingresar una URL de imagen.";
+      hasErrors = true;
+    }
+
+    setErrors(newErrors);
+    return !hasErrors;
   };
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      try {
+        await postCasino(input, tokenId);
+
+        setInput({
+          name: "",
+          image_url: "",
+        });
+
+        onClose();
+        Reload();
+
+        swal({
+          title: "Casino creado correctamente!",
+          icon: "success",
+        });
+      } catch (error) {
+        swal({
+          title: "Error",
+          text: error.message,
+          icon: "error",
+        });
+      }
+    }
+  };
+
   return (
     <div onClick={(e) => e.stopPropagation()}>
       <form className={css.formulario} onSubmit={handlerSubmit}>
         <h2>Crear Casino</h2>
-        <input
-          type="text"
-          name="name"
-          value={input.name}
-          placeholder="Nombre del Casino"
-          onChange={handlerInputChange}
-        />
-        <input
-          type="text"
-          name="image_url"
-          value={input.image_url}
-          placeholder="url de imagen"
-          onChange={handlerInputChange}
-        />
+        <div className={css.formGroup}>
+          <input
+            type="text"
+            name="name"
+            value={input.name}
+            placeholder="Nombre del Casino"
+            onChange={handlerInputChange}
+          />
+          {errors.name && <p className={css.errorMsg}>{errors.name}</p>}
+        </div>
+        <div className={css.formGroup}>
+          <input
+            type="text"
+            name="image_url"
+            value={input.image_url}
+            placeholder="URL de imagen"
+            onChange={handlerInputChange}
+          />
+          {errors.image_url && <p className={css.errorMsg}>{errors.image_url}</p>}
+        </div>
         <button type="submit">Crear</button>
       </form>
     </div>
