6e7dd9e206782c9703ccd6479689640bd955396d
euaragon <eugenio.aragon@gmail.com>
2023-07-24 17:54:20 -0300

    formulario controlado editar cajero

--- app/Cashier/[id]/page.tsx
+++ app/Cashier/[id]/page.tsx
@@ -6,12 +6,12 @@ import { useUserContext } from "../../UserContext/UserContext";
 import { Modal } from "../../Components/modal/modal";
 import { useRouter } from "next/navigation";
 
-const Page = ({params}) => {  
-  const router = useRouter()  
-  const {id} = params; 
-  const { usersDb, charge, setCharge } = useUsersContext(); 
-  const {userDb} = useUserContext();
-  const userSelected = usersDb?.filter(u => u.id === id) 
+const Page = ({ params }) => {
+  const router = useRouter();
+  const { id } = params;
+  const { usersDb, charge, setCharge } = useUsersContext();
+  const { userDb } = useUserContext();
+  const userSelected = usersDb?.filter((u) => u.id === id);
   const tokenId = userDb?.token;
   const [open, setOpen] = React.useState(false);
   const [userEdit, setUserEdit] = React.useState({
@@ -19,7 +19,7 @@ const Page = ({params}) => {
     email: userSelected[0].email,
     percent_agreement: userSelected[0].percent_agreement,
   });
-console.log(userSelected[0])
+  console.log(userSelected[0]);
   const onClose = () => {
     setOpen(!open);
   };
@@ -29,7 +29,7 @@ console.log(userSelected[0])
   };
 
   const transformStatus =
-  userSelected[0].status === "INACTIVE"
+    userSelected[0].status === "INACTIVE"
       ? "INACTIVO"
       : userSelected[0].status === "DISABLED"
       ? "BLOQUEADO"
@@ -43,7 +43,7 @@ console.log(userSelected[0])
         authorization: "Bearer " + tokenId,
       },
     });
-    reload();    
+    reload();
   };
 
   const blockUser = async () => {
@@ -59,7 +59,6 @@ console.log(userSelected[0])
       }),
     });
     reload();
-    
   };
   const changeRole = async () => {
     const rol = userSelected[0].role === "ADMIN" ? "TELLER" : "ADMIN";
@@ -73,7 +72,7 @@ console.log(userSelected[0])
         role: rol,
       }),
     });
-    reload();    
+    reload();
   };
 
   const handlerInputChange = ({
@@ -104,18 +103,64 @@ console.log(userSelected[0])
     });
     reload();
   };
+
+  const [errors, setErrors] = React.useState({
+    phone: "",
+    email: "",
+    percent_agreement: "",
+  });
+
+  const validateForm = () => {
+    let hasErrors = false;
+    const newErrors = {
+      phone: "",
+      email: "",
+      percent_agreement: "",
+    };
+
+    if (!userEdit.phone.trim()) {
+      newErrors.phone = "El campo Teléfono es requerido.";
+      hasErrors = true;
+    }
+
+    if (!userEdit.email.trim()) {
+      newErrors.email = "El campo Correo es requerido.";
+      hasErrors = true;
+    }
+
+    if (
+      userEdit.percent_agreement === "" ||
+      isNaN(userEdit.percent_agreement)
+    ) {
+      newErrors.percent_agreement = "Porcentaje de acuerdo inválido.";
+      hasErrors = true;
+    } else if (
+      userEdit.percent_agreement < 1 ||
+      userEdit.percent_agreement > 100
+    ) {
+      newErrors.percent_agreement = "El porcentaje debe estar entre 1 y 100.";
+      hasErrors = true;
+    }
+
+    setErrors(newErrors);
+    return !hasErrors;
+  };
+
   const handlerSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    editUser(userEdit);    
-    onClose();   
+    if (validateForm()) {
+      editUser(userEdit);
+      onClose();
+    }
   };
+
   return (
     <div className={css.container} onClick={(e) => e.stopPropagation()}>
-      {open ? (        
+      {open ? (
         <div onClick={(e) => e.stopPropagation()}>
           <form className={css.form_container} onSubmit={handlerSubmit}>
             <h2>{userSelected[0].username}</h2>
-           
+
             <input
               type="text"
               name="phone"
@@ -141,11 +186,16 @@ console.log(userSelected[0])
             />
             <button type="submit">Guardar cambios</button>
           </form>
-        </div>       
+
+          <div className={css.errores}>
+            {errors.phone && <p>{errors.phone}</p>}
+            {errors.email && <p>{errors.email}</p>}
+            {errors.percent_agreement && <p>{errors.percent_agreement}</p>}
+          </div>
+        </div>
       ) : (
         <div className={css.datos}>
           <h1>{userSelected[0].username}</h1>
-          
 
           <div className={css.box}>
             <div className={css.box1}>
@@ -166,7 +216,11 @@ console.log(userSelected[0])
                 <h3>
                   <b>Rol:</b>
                 </h3>
-                <h4>{userSelected[0].role === "ADMIN" ? "ADMINISTRADOR" : "CAJERO"}</h4>
+                <h4>
+                  {userSelected[0].role === "ADMIN"
+                    ? "ADMINISTRADOR"
+                    : "CAJERO"}
+                </h4>
               </div>
               <div className={css.data}>
                 <h3>
@@ -189,8 +243,9 @@ console.log(userSelected[0])
                     <h3>
                       <b>{uc.casino.name}</b>
                     </h3>
-                    <h4>Fichas disponibles:
-                    <b>{uc.coins}</b>
+                    <h4>
+                      Fichas disponibles:
+                      <b>{uc.coins}</b>
                     </h4>
                     <br />
                   </div>
@@ -201,19 +256,19 @@ console.log(userSelected[0])
           <div className={css.btn}>
             <button onClick={deleteUser}>Eliminar</button>
             <button onClick={blockUser}>
-              {userSelected[0].status === "DISABLED" ? "Desbloquear" : "Bloquear"}
+              {userSelected[0].status === "DISABLED"
+                ? "Desbloquear"
+                : "Bloquear"}
             </button>
             <button onClick={onClose}>Editar</button>
             <button onClick={changeRole}>Cambiar Rol</button>
           </div>
-          
         </div>
-        
-        )}
-      
-        <button onClick={()=> open ? onClose() : router.back()}>Cerrar</button>
+      )}
+
+      <button onClick={() => (open ? onClose() : router.back())}>Cerrar</button>
     </div>
   );
 };
 
-export default Page
+export default Page;
