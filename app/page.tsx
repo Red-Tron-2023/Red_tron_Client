0245b44b9209c0530df3a69fd0df67d2a5aef5ac
jebenitez92 <jonathan92_24@hotmail.com>
2023-08-14 00:21:04 -0300

    Se realizan validaciones en el formulario de inicio de sesión.

--- app/page.tsx
+++ app/page.tsx
@@ -6,59 +6,117 @@ import React, { useEffect } from "react";
 import { useUserContext } from "./UserContext/UserContext";
 import { useRouter } from "next/navigation";
 import {AiOutlineEye} from 'react-icons/ai'
+import swal from "sweetalert";
 
 export default function Home() {
   const router = useRouter();
   const [input, setInput] = React.useState({
     username: "",
     password: "",
+    usernameError: "",
+    passwordError: "",
   });
   const { userDb, setUserDB } = useUserContext();
   const [showPassword, setShowPassword] = React.useState(false);
+
 
   useEffect(() => {
     userDb.role === "ADMIN" ? router.push("/Admin") : userDb.role === "TELLER" ?router.push("/Teller"): null;
   }, [userDb]);
 
   const handleInputChange = ({ target: { name, value } }) => {
-    setInput({
-      ...input,
-      [name]: value,
-    });
+    if (name === "username" && value.length < 3) {
+      // Validación para el campo de usuario
+      setInput((prevInput) => ({
+        ...prevInput,
+        [name]: value,
+        usernameError: "El usuario debe tener al menos 3 caracteres",
+      }));
+    } else if (name === "password" && value.length < 6) {
+      // Validación para el campo de contraseña
+      setInput((prevInput) => ({
+        ...prevInput,
+        [name]: value,
+        passwordError: "La contraseña debe tener al menos 6 caracteres",
+      }));
+    } else {
+      // Si no hay errores, actualiza el valor normalmente
+      setInput((prevInput) => ({
+        ...prevInput,
+        [name]: value,
+        usernameError: "",
+        passwordError: "",
+      }));
+    }
   };
-
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
   const userLogin = async (input:object) => {
-    const response = await fetch("https://redtronapi-development.up.railway.app/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(input),
-    })
+    try {
+      const response = await fetch("https://redtronapi-development.up.railway.app/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(input),
+      })
       .then((res) => res.json())
       .then((response) => {
-        let user = {
-          id: response?.data.id,
-          username: response?.data.username,
-          role: response?.data.role,
-          phone: response?.data.phone,
-          email: response?.data.email,
-          token: response?.data.token,
-          status: response?.data.status
-        };
-        return user;
-      });
-    setUserDB(response);
+        console.log(response)
+        if(response.error){
+          swal({
+            title: "Error",
+            text: response.message,
+            icon: "error",
+          });
+          throw new Error(response.message);
+        }
+          let user = {
+            id: response?.data.id,
+            username: response?.data.username,
+            role: response?.data.role,
+            phone: response?.data.phone,
+            email: response?.data.email,
+            token: response?.data.token,
+            status: response?.data.status
+          };
+          return user;
+        });
+      setUserDB(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    userLogin(input);
+    try {
+      e.preventDefault();
+      //Verifico si tiene errores el formulario antes de enviarlo
+      if(input.passwordError){
+        swal({
+          title: "Error",
+          text: input.passwordError,
+          icon: "error",
+        });
+        throw new Error(input.passwordError);
+      }else if(input.usernameError){
+        swal({
+          title: "Error",
+          text: input.usernameError,
+          icon: "error",
+        });
+        throw new Error(input.usernameError);
+      }else{
+        //Si no encuentra error se envia
+        userLogin(input);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
 
   return (
     <main>
@@ -73,6 +131,10 @@ export default function Home() {
           onChange={handleInputChange}
           required
         />
+        <div>
+        {input.usernameError && <p style={{ color: 'red' }} >{input.usernameError}</p>}
+        </div>
+        <AiOutlineEye className="eye" onClick={toggleShowPassword}/>
         <input
           type={showPassword ? "text" : "password"} // Cambia el tipo del input
           placeholder="Password"
@@ -81,11 +143,14 @@ export default function Home() {
           onChange={handleInputChange}
           required
         />
-        <AiOutlineEye className="eye" onClick={toggleShowPassword}/>
+        <div>
+        {input.passwordError && <p style={{ color: 'red' }} >{input.passwordError}</p>}
+        </div>
         <button type="submit">ENTRAR</button>
       </form>
     </main>
   );
 }
+
 
 
