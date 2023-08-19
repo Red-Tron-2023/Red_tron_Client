99e3d42738ba46121ca2d3b3f47b7bb1f1163609
María Emilia Landa <emilia.landa@gmail.com>
2023-08-19 19:29:00 -0300

    renderizado de casinos en teller y page casinoTeller

---
+++ app/CasinoTeller/[id]/page.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+export default function Page () {
+
+  return (
+    <div>
+      <h1>CREAR JUGADOR</h1>
+      {/* Otro contenido de la página */}
+    </div>
+  );
+};
+
